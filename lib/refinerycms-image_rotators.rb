2140b8f447be1c6bfb4c2194db07109ee242c4dd
Jacob Swanner <jacob@envylabs.com>
2011-04-07 14:04:42 -0400

    no longer overriding the entire image class, just adding association thru class_eval

--- lib/refinerycms-image_rotators.rb
+++ lib/refinerycms-image_rotators.rb
@@ -7,12 +7,18 @@ module Refinery
       initializer "static assets" do |app|
         app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
       end
-      
+
       config.after_initialize do
         Refinery::Plugin.register do |plugin|
           plugin.name = "image_rotators"
           plugin.menu_match = /(admin|refinery)\/(image_rotators|image_rotator_images)$/
           plugin.activity = {:class => ImageRotator, :title => 'title'}
+        end
+      end
+
+      config.to_prepare do
+        Image.class_eval do
+          has_many :image_rotator_images, :dependent => :destroy
         end
       end
     end
