d58dddcbb69d463d16478dfc1ad720f1e1018b3a
Jacob Swanner <jacob@envylabs.com>
2011-06-29 18:33:57 -0400

    no longer require a url for an image

--- app/models/image_rotator_image.rb
+++ app/models/image_rotator_image.rb
@@ -5,8 +5,6 @@ class ImageRotatorImage < ActiveRecord::Base
   belongs_to :image_rotator
   belongs_to :image
 
-  validates :url, :presence => true
-
   acts_as_indexed :fields => [:url]
   acts_as_list
 
